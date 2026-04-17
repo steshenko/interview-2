e00a5edbaea0905862bdd00e0748d81026d70df6
andrii <steshenko.com@gmail.com>
2026-04-17 19:53:34 +0300

    Configure GitHub Pages deployment

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
 
   return (
       <QueryClientProvider client={queryClient}>
-          <BrowserRouter>
+          <BrowserRouter basename="/interview-2">
             <Index/>
             <AppRouter/>
           </BrowserRouter>
